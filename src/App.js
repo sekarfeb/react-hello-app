a91e99982c9f0fa18ba2fc33328e276ecffe80a4
sekarfeb <sekarfeb@gmail.com>
2023-06-28 18:41:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome <code> To Kanini</code> Internal Project.
+          Welcome <code> To Kanini</code> Internal Project (Deploy React App to Kubernetes).
         </p>
         <a
           className="App-link"
